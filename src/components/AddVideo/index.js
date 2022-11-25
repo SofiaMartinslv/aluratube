4c2cc2dadd2cf058030bebd936deafb9f33f90c1
Sofia <sofia.martinslv@gmail.com>
2022-11-25 13:02:43 -0300

    :construction:

--- src/components/AddVideo/index.js
+++ src/components/AddVideo/index.js
@@ -25,6 +25,7 @@ export default function AddVideo(props) {
     initialValues: { title: "", url: "", playlist: "" },
   });
   const [showModal, setShowModal] = React.useState(false);
+  const [showAddPlaylist, setShowAddPlaylist] = React.useState(false);
   const getThumbUrl = function (videoUrl) {
     if (videoUrl) {
       const filter = videoUrl.match("[\\?&]v=([^&#]*)");
@@ -82,20 +83,71 @@ export default function AddVideo(props) {
                 value={addVideoForm.values.url}
                 onChange={addVideoForm.handleChange}
               />
-              <label className="label pb-0">
-                <span className="label-text">Playlist</span>
-              </label>
-              <div className="flex w-full">
-                <select className="select select-bordered flex-auto"  name="playlist" value={addVideoForm.values.playlist} onChange={addVideoForm.handleChange}>
-                  {props.playlists.length > 1 &&
-                    props.playlists.map((playlist) => {
-                      return (
-                          <option value={playlist.id} key={playlist.id}>{playlist.name}</option>
-                      );
-                    })}
-                </select>
-                <button className="w-14 ml-2 red-button btn tooltip tooltip-left" data-tip="Add playlist">+</button>
-              </div>
+              {!showAddPlaylist && (
+                <>
+                  <label className="label pb-0">
+                    <span className="label-text">Playlist</span>
+                  </label>
+                  <div className="flex w-full">
+                    <select
+                      className="select select-bordered flex-auto"
+                      name="playlist"
+                      value={addVideoForm.values.playlist}
+                      onChange={addVideoForm.handleChange}
+                    >
+                      {props.playlists.length > 1 &&
+                        props.playlists.map((playlist) => {
+                          return (
+                            <option value={playlist.id} key={playlist.id}>
+                              {playlist.name}
+                            </option>
+                          );
+                        })}
+                    </select>
+                    <button
+                      type="button"
+                      onClick={() => {
+                        setShowAddPlaylist(true);
+                      }}
+                      className="w-14 ml-2 red-button btn tooltip tooltip-left"
+                      data-tip="Add playlist"
+                    >
+                      +
+                    </button>
+                  </div>
+                </>
+              )}
+              {showAddPlaylist && (
+                <>
+                  <label className="label pb-0">
+                    <span className="label-text">Add playlist</span>
+                  </label>
+                  <div className="flex">
+                    <input
+                      className="input w-full"
+                      placeholder="Playlist"
+                      name="playlist"
+                    />
+                    <div className="flex">
+                      <button
+                        type="button"
+                        onClick={() => {
+                          setShowAddPlaylist(false);
+                        }}
+                        className="w-14 ml-2 red-button btn"
+                      >
+                        X
+                      </button>
+                      <button
+                        type="button"
+                        className="w-14 ml-2 red-button btn"
+                      >
+                        Add
+                      </button>
+                    </div>
+                  </div>
+                </>
+              )}
               <label className="label pb-0">
                 <span className="label-text">Thumbnail</span>
               </label>
